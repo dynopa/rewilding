76a274908fa67f8e08f0213956296991ae566a33
rowanq <rowanqueue@gmail.com>
2020-03-09 08:05:01 -0400

    multiple saves working

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,6 +6,7 @@ using System;
 
 public class GameController : MonoBehaviour
 {
+    public byte saveId;
     public TextMeshProUGUI dayCounter;
     public TextureEditor texEdit;
 
@@ -15,6 +16,7 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        saveId = 1;
         date = DateTime.Now;
         InitializeServices();
     }
@@ -22,6 +24,36 @@ public class GameController : MonoBehaviour
         dayCounter.text = date.Year +" "+months[date.Month-1];
         if(Input.GetKeyDown(KeyCode.F)){
             SaveLoad.Load();
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha1)){
+            saveId = 1;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2)){
+            saveId = 2;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha3)){
+            saveId = 3;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha4)){
+            saveId = 4;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha5)){
+            saveId = 5;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha6)){
+            saveId = 6;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha7)){
+            saveId = 7;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha8)){
+            saveId = 8;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha9)){
+            saveId = 9;
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha0)){
+            saveId = 0;
         }
         
     }
