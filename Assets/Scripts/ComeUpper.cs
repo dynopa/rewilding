f369c018081b86e4ca76e07932f6f546c4450500
rowanq <rowanqueue@gmail.com>
2020-04-28 23:34:59 -0400

    plants now rise up when planted or loaded in; player now has locked plants

---
+++ Assets/Scripts/ComeUpper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComeUpper : MonoBehaviour
+{
+    Vector3 targetPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPos = transform.position;
+        transform.position += Vector3.down*2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (targetPos-transform.position)*0.25f;
+        if(Vector3.Distance(targetPos,transform.position) < 0.005f){
+            Destroy(this);
+        }
+    }
+}
