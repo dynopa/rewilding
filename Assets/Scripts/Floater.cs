e519b094db124417f4122ae8f6d6298bcae3d39a
rowanq <rowanqueue@gmail.com>
2020-05-04 15:23:14 -0400

    narrative moments

---
+++ Assets/Scripts/Floater.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Floater : MonoBehaviour
+{
+    Vector3 startPos;
+    float startTime;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        startPos = transform.localPosition;
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition = startPos + Vector3.up*Mathf.Sin(startTime+Time.time*2f)/5f;
+    }
+}
