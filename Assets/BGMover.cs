07fe714599f41e27cec4efdbf4273614a7827e98
Spencer Bernstein <>
2020-05-09 09:03:44 -0400

    Movin' menus

---
+++ Assets/BGMover.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMover : MonoBehaviour
+{
+    public Vector2 pos1;
+    public Vector2 pos2;
+    private Vector2 startPos;
+    private float lerpStartTime;
+    public float lerpLength;
+    private RectTransform rTransform;
+    private float timePassed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rTransform = gameObject.GetComponent<RectTransform>();
+        startPos = rTransform.anchoredPosition;
+        pos1 = startPos;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timePassed += Time.deltaTime/2;
+        // Distance moved equals elapsed time times speed..
+        float distCovered = (Time.time - lerpStartTime) * 2f;
+        float squatProg = distCovered / lerpLength;
+        rTransform.anchoredPosition = Vector2.Lerp(pos1, pos2, Mathf.SmoothStep(0, 1, Mathf.PingPong(Time.time / lerpLength, 1)));
+        
+        if (squatProg > 1)
+        {
+            
+        }
+    }
+}
